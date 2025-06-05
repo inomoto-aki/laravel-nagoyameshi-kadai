25849fc047e2c59c1436690cdd9146a188f94a7a
inomoto-aki <aki140323@outlook.jp>
2025-06-05 23:54:02 +0900

    会員側の認証を実装

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -8,7 +8,7 @@ use App\Models\Category;
 
 class RestaurantController extends Controller
 {
-    public function index(Request $request){
+     public function index(Request $request) {
         $keyword = $request->input('keyword');
 
         $category_id = $request->input('category_id');
@@ -20,31 +20,31 @@ class RestaurantController extends Controller
             '価格が安い順' => 'lowest_price asc'
         ];
 
-        $sort_quey = [];
+        $sort_query = [];
         $sorted = 'created_at desc';
 
-        if($request->has('select_sort')){
-            $slices = explode('',$request->input('select_sort'));
+        if ($request->has('select_sort')) {
+            $slices = explode(' ', $request->input('select_sort'));
             $sort_query[$slices[0]] = $slices[1];
             $sorted = $request->input('select_sort');
         }
 
-        if($keyword){
-            $restaurants = Restaurant::where('name', 'like', "%{keyword}%")
-            ->orwhere('address', 'like', "%{$keyword}%")
-            ->orwhereHas('categories', function ($query) use($keyword){
-                $query->where('categories.name', 'like', "%{$keyword}%");
-            })
-            ->sortable($sort_query)
-            ->orderBy('created_at', 'desc')
-            ->paginate(15);
-        }elseif($category_id){
-            $restaurants = Restaurant::whereHas('categories', function ($query) use ($category_id){
+        if ($keyword) {
+            $restaurants = Restaurant::where('name', 'like', "%{$keyword}%")
+                ->orWhere('address', 'like', "%{$keyword}%")
+                ->orWhereHas('categories', function ($query) use ($keyword) {
+                    $query->where('categories.name', 'like', "%{$keyword}%");
+                })
+                ->sortable($sort_query)
+                ->orderBy('created_at', 'desc')
+                ->paginate(15);
+        } elseif ($category_id) {
+            $restaurants = Restaurant::whereHas('categories', function ($query) use ($category_id) {
                 $query->where('categories.id', $category_id);
             })->sortable($sort_query)->orderBy('created_at', 'desc')->paginate(15);
-        }elseif ($price){
+        } elseif ($price) {
             $restaurants = Restaurant::where('lowest_price', '<=', $price)->sortable($sort_query)->orderBy('created_at', 'desc')->paginate(15);
-        }else{
+        } else {
             $restaurants = Restaurant::sortable($sort_query)->orderBy('created_at', 'desc')->paginate(15);
         }
 
@@ -52,6 +52,6 @@ class RestaurantController extends Controller
 
         $total = $restaurants->total();
 
-        return view('restaurants.index', compact('keyword', 'category_id','price', 'sorts','restaurants','categories', 'total'));
+        return view('restaurants.index', compact('keyword', 'category_id', 'price', 'sorts', 'sorted', 'restaurants', 'categories', 'total'));
     }
 }
