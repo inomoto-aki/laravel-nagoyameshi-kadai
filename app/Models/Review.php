15d3e11fbb7df949b9bb7d3a48da8be8da679e1b
inomoto-aki <aki140323@outlook.jp>
2025-06-08 23:41:43 +0900

    会員側の認証を実装

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -9,6 +9,9 @@ class Review extends Model
 {
     use HasFactory;
 
+    public function restaurant(){
+        return $this->belongsTo(Restaurant::class);
+    }
     public function user(){
         return $this->belongsTo(User::class);
     }
