3d604685c47b94602d495f23d5d454fdc9732901
inomoto-aki <aki140323@outlook.jp>
2025-06-10 22:26:40 +0900

    会員側の認証を実装

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -25,7 +25,9 @@ class Restaurant extends Model
     public function reservations(){
         return $this->hasMany(Reservation::class);
     }
-
+    public function favorited_users() {
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
     public function ratingSortable($query, $direction){
         return $query->withAvg('reviews', 'score')->orderBy('reviews_avg_score', $direction);
     }
